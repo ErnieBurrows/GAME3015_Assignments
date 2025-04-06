3059e24fee48c86900758df177d4d02437764223
ErnieBurrows <114937798+ErnieBurrows@users.noreply.github.com>
2025-04-06 17:45:37 -0400

    Addded Dynamic console creation in main

--- Assignment1/Solution/Project/main.cpp
+++ Assignment1/Solution/Project/main.cpp
@@ -1,8 +1,31 @@
 #include "Game.hpp"
 
+#include <windows.h>   // For AllocConsole
+#include <cstdio>      // For freopen
+#include <iostream>    // For std::cout
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	PSTR cmdLine, int showCmd)
 {
+
+#ifdef _DEBUG
+	AllocConsole();
+
+	// Redirect STDOUT
+	FILE* newStdout;
+	freopen_s(&newStdout, "CONOUT$", "w", stdout);
+
+	// Redirect STDIN
+	FILE* newStdin;
+	freopen_s(&newStdin, "CONIN$", "r", stdin);
+
+	// Redirect STDERR
+	FILE* newStderr;
+	freopen_s(&newStderr, "CONOUT$", "w", stderr);
+
+	std::cout << "Console successfully allocated!\n";
+#endif
+
 	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);

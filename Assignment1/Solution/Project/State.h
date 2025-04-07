fb378761f76dd4fb1f6f42c528f186b787ada717
ErnieBurrows <114937798+ErnieBurrows@users.noreply.github.com>
2025-04-07 03:11:42 -0400

    Refactored state class
    Now holds reference to a player

--- Assignment1/Solution/Project/State.h
+++ Assignment1/Solution/Project/State.h
@@ -1,9 +1,15 @@
 #pragma once
+
+#include "../../Common/d3dApp.h"
+#include "FrameResource.h"
+#include "SceneNode.hpp"
 #include <memory>
 #include<Windows.h>
 
 class StateStack;
 class Game;
+class Player;
+class SceneNode;
 
 class State
 {
@@ -13,26 +19,38 @@ public:
 	// Context struct to pass information and pointers to each state
 	struct Context
 	{
+
+		Context(Game* _game, Player* _player);
 		Game* game;
+		Player* player;
 	};
 
-	State(StateStack& stack, Context context) : mStack(&stack), mContext(context) {}
+	State(StateStack& stack, Context context);
 
-	virtual ~State() {}
+	virtual ~State();
 
 	virtual void Draw() = 0;
-	virtual bool Update(float dt) = 0;
+	virtual bool Update(const GameTimer& gt) = 0;
 	virtual bool HandleEvent(WPARAM btnState, int x, int y) = 0;
+
+	virtual bool IsTransparent() const { return false; }
+
+	std::vector<std::unique_ptr<RenderItem>>& getRenderItems() { return mAllRitems; }
+
 
 protected:
 	void RequestStackPush(int stateID);
 	void RequestStackPop();
 	void RequestStackClear();
 
+	
 	Context GetContext() const { return mContext; }
 
 private:
 	StateStack* mStack;
 	Context mContext;
+
+	std::unique_ptr<SceneNode> mSceneGraph; 
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems; 
 };
 

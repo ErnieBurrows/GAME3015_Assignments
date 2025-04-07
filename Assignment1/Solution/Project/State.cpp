c578a8dc3dc3bdff91b531594dbe6ad8a7640819
ErnieBurrows <114937798+ErnieBurrows@users.noreply.github.com>
2025-04-07 12:40:48 -0400

    Reapply "Refactored state class"
    This reverts commit 2c7d6b365910171b8480e94e2fa68963a324c366.

--- Assignment1/Solution/Project/State.cpp
+++ Assignment1/Solution/Project/State.cpp
@@ -1,6 +1,17 @@
 #include "State.h"
 #include "StateStack.h"
 
+State::Context::Context(Game* _game, Player* _player) : game(_game), player(_player)
+{
+}
+
+State::State(StateStack& stack, Context context) : mStack(&stack)
+, mContext(context), mSceneGraph(std::make_unique<SceneNode>(this)) 
+{ 
+}
+
+State::~State() { }
+
 void State::RequestStackPush(int stateID)
 {
 	mStack->PushState(stateID);
@@ -15,3 +26,5 @@ void State::RequestStackClear()
 {
 	mStack->ClearStates();
 }
+
+

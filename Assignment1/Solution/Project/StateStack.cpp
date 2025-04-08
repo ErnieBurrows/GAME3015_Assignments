416e3f86724b75226656a91334102207d5507183
ErnieBurrows <114937798+ErnieBurrows@users.noreply.github.com>
2025-04-08 06:30:55 -0400

    Refactored state stack class

--- Assignment1/Solution/Project/StateStack.cpp
+++ Assignment1/Solution/Project/StateStack.cpp
@@ -1,1 +1,87 @@
 #include "StateStack.h"
+
+void StateStack::Update(const GameTimer& gt)
+{
+	// Upate from the top state downward until one returns false
+	for (auto i = mStack.rbegin(); i != mStack.rend(); ++i)
+	{
+		if (!(*i)->Update(gt))
+			break;
+	}
+	ApplyPendingChanges();
+}
+
+void StateStack::Draw()
+{
+	for (State::Ptr& state : mStack)
+	{
+		state->Draw();
+	}
+}
+
+void StateStack::HandleEvent(WPARAM btnState, int x, int y)
+{
+	for (auto i = mStack.rbegin(); i != mStack.rend(); ++i)
+	{
+		if (!(*i)->HandleEvent(btnState, x, y))
+			break;
+	}
+	ApplyPendingChanges();
+}
+
+void StateStack::PushState(int stateID)
+{
+	mPendingList.push_back({ Push, stateID });
+}
+
+void StateStack::PopState()
+{
+	mPendingList.push_back({ Pop, 0 });
+}
+
+void StateStack::ClearStates()
+{
+	mPendingList.push_back({ Clear, 0 });
+}
+
+bool StateStack::IsEmpty() const
+{
+	return mStack.empty();
+}
+
+State::Ptr StateStack::CreateState(int stateID)
+{
+	auto found = mFactories.find(stateID);
+
+	if (found == mFactories.end())
+	{
+		throw std::runtime_error("State not registered");
+	}
+
+	return found->second();
+}
+
+void StateStack::ApplyPendingChanges()
+{
+	for (PendingChange change : mPendingList)
+	{
+		switch (change.action)
+		{
+		case Push:
+			mStack.push_back(CreateState(change.stateID));
+			break;
+
+		case Pop:
+			if (!mStack.empty())
+				mStack.pop_back();
+			break;
+
+		case Clear:
+			mStack.clear();
+			break;
+		}
+
+	}
+	// Clear the pending changes after applying them
+	mPendingList.clear();
+}
--- Assignment1/Solution/Project/StateStack.h
+++ Assignment1/Solution/Project/StateStack.h
@@ -1,128 +1,54 @@
-#pragma once
-#include "State.h"
-
-#include <vector>
-#include <functional>
+#pragma once  
+#include "State.h"  
+#include <vector>  
+#include <functional>  
+#include <utility>  
 #include <map>
 #include <stdexcept>
 
-class StateStack
-{
-public:
-	enum Action
-	{
-		Push,
-		Pop,
-		Clear
-	};
+class StateStack  
+{  
+public:  
+   enum Action  
+   {  
+       Push,  
+       Pop,  
+       Clear  
+   };  
 
-	explicit StateStack(State::Context context) : mContext(context) {}
+   explicit StateStack(State::Context context) : mContext(context) {}  
 
-	template<typename T>
-	void RegisterState(int stateID)
-	{
-		mFactories[stateID] = [this]() 
-		{
-			return State::Ptr(new T(*this, mContext));
-		};
-	}
+   template<typename T>  
+   void RegisterState(int stateID)  
+   {  
+       mFactories[stateID] = [this]()  
+       {  
+           return State::Ptr(new T(*this, mContext));  
+       };  
+   }  
 
-	void Update(const GameTimer& gt)
-	{
-		// Upate from the top state downward until one returns false
-		for (auto i = mStack.rbegin(); i != mStack.rend(); ++i)
-		{
-			if (!(*i)->Update(gt))
-				break;
-		}
-		ApplyPendingChanges();
-	}
+   void Update(const GameTimer& gt);  
+   void Draw();  
+   void HandleEvent(WPARAM btnState, int x, int y);  
+   void PushState(int stateID);  
+   void PopState();  
+   void ClearStates();  
+   bool IsEmpty() const;  
 
-	void Draw()
-	{
-		for (State::Ptr& state : mStack)
-		{
-			state->Draw();
-		}
-	}
+   struct PendingChange
+   {
+       Action action;
+       int stateID;
+   };
 
-	void HandleEvent(WPARAM btnState, int x, int y)
-	{
-		for (auto i = mStack.rbegin(); i != mStack.rend(); ++i)
-		{
-			if (!(*i)->HandleEvent(btnState, x, y))
-				break;
-		}
-		ApplyPendingChanges();
-	}
+private:  
+    
 
-	void PushState(int stateID)
-	{
-		mPendingList.push_back({Push, stateID});
-	}
+   State::Ptr CreateState(int stateID);  
+   void ApplyPendingChanges();  
 
-	void PopState()
-	{
-		mPendingList.push_back({Pop, 0});
-	}
-
-	void ClearStates()
-	{
-		mPendingList.push_back({ Clear, 0 });
-	}
-
-	bool IsEmpty() const
-	{
-		return mStack.empty();
-	}
-
-private:
-	struct PendingChange
-	{
-		Action action;
-		int stateID;
-	};
-
-	State::Ptr CreateState(int stateID)
-	{
-		auto found = mFactories.find(stateID);
-
-		if (found == mFactories.end())
-		{
-			throw std::runtime_error("State not registered");
-		}
-
-		return found->second();
-	}
-
-	void ApplyPendingChanges()
-	{
-		for (PendingChange change : mPendingList)
-		{
-			switch (change.action)
-			{
-			case Push:
-				mStack.push_back(CreateState(change.stateID));
-				break;
-
-			case Pop:
-				if (!mStack.empty())
-					mStack.pop_back();
-				break;
-
-			case Clear:
-				mStack.clear();
-				break;
-			}
-
-		}
-		// Clear the pending changes after applying them
-		mPendingList.clear();
-	}
-
-	std::vector<State::Ptr> mStack;
-	std::vector<PendingChange> mPendingList;
-	State::Context mContext;
-	std::map<int, std::function<State::Ptr()>> mFactories;
+   std::vector<State::Ptr> mStack;
+   std::vector<PendingChange> mPendingList;
+   State::Context mContext;
+   std::map<int, std::function<State::Ptr()>> mFactories;
 };
-
